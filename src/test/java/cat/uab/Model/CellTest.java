90fa1196828366d0eb29983be94f79c78ee24bfa
NIU1708365 <1708365@uab.cat>
2025-11-10 11:41:04 +0100

    Test reveal

--- src/test/java/cat/uab/Model/CellTest.java
+++ src/test/java/cat/uab/Model/CellTest.java
@@ -20,15 +20,25 @@ public class CellTest {
 
     @Test
 
-    public void test_ConstructorP(){
+    public void test_ConstructorP() {
         Cell cell = new Cell(true);
         assertTrue(cell.getIsMine());
         assertFalse(cell.getIsFlagged());
         assertFalse(cell.getIsRevealed());
-        
 
     }
 
-    
+    @Test
+    public void test_reveal() {
+        Cell cell = new Cell();
+        assertFalse(cell.getIsRevealed());
+        assertFalse(cell.reveal());
+        assertTrue(cell.getIsRevealed());
+
+        // mirem que segueix sent true
+        assertFalse(cell.reveal());
+        assertTrue(cell.getIsRevealed());
+
+    }
 
 }
