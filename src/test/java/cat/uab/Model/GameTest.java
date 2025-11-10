863d5720819e30ce07e0c05a3fc0e624544ada37
NIU1708365 <1708365@uab.cat>
2025-11-10 12:22:35 +0100

    Test del Rang

--- src/test/java/cat/uab/Model/GameTest.java
+++ src/test/java/cat/uab/Model/GameTest.java
@@ -1,4 +1,5 @@
 package cat.uab.Model;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -7,26 +8,51 @@ import org.junit.jupiter.api.Test;
 import cat.uab.RandomNumberGenerator;
 
 public class GameTest {
-    
-    //Tests TDD
+
+    // Tests TDD
     @Test
-    public void test_CreacioTaulerCorrectament()
-    {
+    public void test_Constructor() {
         RandomNumberGenerator rng = new RandomNumberGenerator();
 
-        Game game = new Game(4,5,3,rng);
-        
-        assertEquals(4,game.getRows());
+        Game game = new Game(4, 5, 3, rng);
+
+        assertEquals(4, game.getRows());
         assertEquals(5, game.getColumns());
         assertEquals(3, game.getNumMines());
-        
+
     }
 
+    @Test
+    public void test_reveal_Range() {
+        RandomNumberGenerator rng = new RandomNumberGenerator();
+        Game game = new Game(4, 5, 3, rng);
 
+        try {
+            game.reveal(-1, -1);
+            fail("Expected an exception");
+        } catch (IllegalArgumentException e) {
+            // pass
+            // esperes error
+        }
 
+        catch (Exception e) {
+            fail("Expected an IllegalArgumentException");
+        }
 
-    
+        try {
+            game.reveal(4, 5);
+            fail("Expected an exception");
+        } catch (IllegalArgumentException e) {
+            // pass
+            // esperes error
+        }
 
+        catch (Exception e) {
+            fail("Expected an IllegalArgumentException");
+        }
 
+        game.reveal(0, 0);
+
+    }
 
 }
