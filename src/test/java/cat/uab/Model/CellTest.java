27a02e7461f9beb01d3ff773aa34f396d2930a92
NIU1702313 <Adam.Jimenez@autonoma.cat>
2025-12-01 17:27:48 +0100

    Resolts els conflictes

--- src/test/java/cat/uab/Model/CellTest.java
+++ src/test/java/cat/uab/Model/CellTest.java
@@ -6,12 +6,7 @@ import org.junit.jupiter.api.Test;
 
 public class CellTest {
     @Test
-<<<<<<< HEAD
     public void test_constructor() {
-=======
-
-    public void test_Constructor() {
->>>>>>> 81a64349bd34833b2f163f7e841bf4a16beeb907
         Cell cell = new Cell();
         assertFalse(cell.getIsFlagged());
         assertFalse(cell.getIsMine());
@@ -19,20 +14,11 @@ public class CellTest {
     }
 
     @Test
-<<<<<<< HEAD
     public void test_constructorWithParameters() {
-=======
-
-    public void test_ConstructorP() {
->>>>>>> 81a64349bd34833b2f163f7e841bf4a16beeb907
         Cell cell = new Cell(true);
         assertTrue(cell.getIsMine());
         assertFalse(cell.getIsFlagged());
         assertFalse(cell.getIsRevealed());
-<<<<<<< HEAD
-=======
-
->>>>>>> 81a64349bd34833b2f163f7e841bf4a16beeb907
     }
 
     @Test
@@ -42,15 +28,8 @@ public class CellTest {
         assertFalse(cell.reveal());
         assertTrue(cell.getIsRevealed());
 
-<<<<<<< HEAD
         assertFalse(cell.reveal());
         assertTrue(cell.getIsRevealed());
-=======
-        // mirem que segueix sent true
-        assertFalse(cell.reveal());
-        assertTrue(cell.getIsRevealed());
-
->>>>>>> 81a64349bd34833b2f163f7e841bf4a16beeb907
     }
 
     @Test
@@ -60,16 +39,7 @@ public class CellTest {
         cell.toggleFlag();
         assertTrue(cell.getIsFlagged());
 
-<<<<<<< HEAD
         cell.toggleFlag();
         assertFalse(cell.getIsFlagged());
     }
-=======
-        // mirem que s'ha girat
-        cell.toggleFlag();
-        assertFalse(cell.getIsFlagged());
-
-    }
-
->>>>>>> 81a64349bd34833b2f163f7e841bf4a16beeb907
 }
